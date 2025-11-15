4f66fd47200ef486c0c748ef4db1c919376006c2
mrgund <70555834+mrgund@users.noreply.github.com>
2025-11-14 23:28:11 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -24,7 +24,7 @@ opkg install --force-overwrite \
     iptables-nft nftables nftables-json \
     iptables-mod-ipopt iptables-mod-nat-extra \
     openvpn-openssl luci-app-openvpn \
-    nginx-ssl openssl-util \
+    nginx-ssl openssl-util easy-rsa \
     fail2ban kmod-tun \
     curl ca-certificates
 
